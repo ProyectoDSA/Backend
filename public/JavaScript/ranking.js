e7f1fb84a78cb8f73dd09fdc864a4fe94c89c0c6
irega97 <ivanreq97@gmail.com>
2020-06-01 00:45:11 +0200

    prueba desarrollo

--- public/JavaScript/ranking.js
+++ public/JavaScript/ranking.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     $("#login").click(function(){
         $('table tbody').empty();
-        $.get("http://localhost:8080/dsaApp/usermanager/users/" + $("#user").val(), function(Object, status) {
+        $.get("http://147.83.7.203:8080/dsaApp/usermanager/users/" + $("#user").val(), function(Object, status) {
             for (var i = 0; i < 1; i++) {
                 $("table").append("<tr><td>"+Object.id+ "</td><td>"+Object.puntuacion+"</td></tr>");}
         });
