6ea64773b96d3c2d51ed69ed335b21b2cb6394c8
vcarl <vcarlos139@gmail.com>
2020-06-10 17:32:29 +0200

    tienda web de locos

--- public/JavaScript/shop.js
+++ public/JavaScript/shop.js
@@ -1,84 +1,101 @@
-var BASE_URI="http://147.83.7.203:8080/dsaApp/game";
+var BASE_URI="http://localhost:8080/dsaApp/game";
 
 $(document).ready(function() {
     //var idJugador = JSON.parse(sessionStorage.getItem("token")).token;
     var idJugador = sessionStorage.getItem("token");
     console.log(idJugador);
-    var cantidad1 = $("#numero1").val();
-    var cantidad2 = $("#numero2").val();
-    var cantidad3 = $("#numero3").val();
-    var cantidad4 = $("#numero4").val();
-    var cantidad5 = $("#numero5").val();
-    var cantidad6 = $("#numero6").val();
+    var cantidad1 = parseInt($("#numero1").text());
+    var cantidad2 = parseInt($("#numero2").text());
+    var cantidad3 = parseInt($("#numero3").text());
+    var cantidad4 = parseInt($("#numero4").text());
+    var cantidad5 = parseInt($("#numero5").text());
+    var cantidad6 = parseInt($("#numero6").text());
     $('#s1').click(function (){
-        cantidad1 + 1;
+        cantidad1 = cantidad1 + 1;
+        $("#numero1").text(cantidad1);
     })
     $('#r1').click(function () {
-        if(cantidad1 != 1) cantidad1 - 1;
-
+        if(cantidad1 != 1) cantidad1 = cantidad1 - 1;
+        else alert("Noooooo");
+        $("#numero1").text(cantidad1);
     })
     $('#s2').click(function (){
-        cantidad2 + 1;
+        cantidad2= cantidad2 + 1;
+        $("#numero2").text(cantidad2);
     })
     $('#r2').click(function () {
-        if(cantidad2 != 1) cantidad2 - 1;
+        if(cantidad2 != 1) cantidad2 = cantidad2 - 1;
+        else alert("Noooooo");
+        $("#numero2").text(cantidad2);
 
     })
     $('#s3').click(function (){
-        cantidad3 + 1;
+        cantidad3= cantidad3 + 1;
+        $("#numero3").text(cantidad3);
     })
     $('#r3').click(function () {
-        if(cantidad3 != 1) cantidad3 - 1;
+        if(cantidad3 != 1) cantidad3 = cantidad3 - 1;
+        else alert("Noooooo");
+        $("#numero3").text(cantidad3);
 
     })
     $('#s4').click(function (){
-        cantidad4 + 1;
+        cantidad4= cantidad4 + 1;
+        $("#numero4").text(cantidad4);
     })
     $('#r4').click(function () {
-        if(cantidad4 != 1) cantidad4 - 1;
+        if(cantidad4 != 1) cantidad4 = cantidad4 - 1;
+        else alert("Noooooo");
+        $("#numero4").text(cantidad4);
 
     })
     $('#s5').click(function (){
-        cantidad5 + 1;
+        cantidad5= cantidad5 + 1;
+        $("#numero5").text(cantidad5);
     })
     $('#r5').click(function () {
-        if(cantidad5 != 1) cantidad5 - 1;
+        if(cantidad5 != 1) cantidad5 = cantidad5 - 1;
+        else alert("Noooooo");
+        $("#numero5").text(cantidad5);
 
     })
     $('#s6').click(function (){
-        cantidad6 + 1;
+        cantidad6= cantidad6 + 1;
+        $("#numero6").text(cantidad6);
     })
     $('#r6').click(function () {
-        if(cantidad6 != 1) cantidad6 - 1;
+        if(cantidad6 != 1) cantidad6 = cantidad6 - 1;
+        else alert("Noooooo");
+        $("#numero6").text(cantidad6);
 
     })
     $('#des1').click(function () {
-        var inventario = {"idObjeto": 1, "cantidad": 1, "idJugador": idJugador};
+        var inventario = {"idObjeto": 1, "cantidad": cantidad1, "idJugador": idJugador};
         console.log(inventario);
         compraObjeto(inventario);
     });
     $('#des2').click(function () {
-        var inventario = {"idObjeto": 2, "cantidad": 1, "idJugador": idJugador};
+        var inventario = {"idObjeto": 2, "cantidad": cantidad2, "idJugador": idJugador};
         console.log(inventario);
         compraObjeto(inventario);
     });
     $('#des3').click(function () {
-        var inventario = {"idObjeto": 3, "cantidad": 1, "idJugador": idJugador};
+        var inventario = {"idObjeto": 3, "cantidad": cantidad3, "idJugador": idJugador};
         console.log(inventario);
         compraObjeto(inventario);
     });
     $('#masc1').click(function () {
-        var inventario = {"idObjeto": 4, "cantidad": 1, "idJugador": idJugador};
+        var inventario = {"idObjeto": 4, "cantidad": cantidad4, "idJugador": idJugador};
         console.log(inventario);
         compraObjeto(inventario);
     });
     $('#masc2').click(function () {
-        var inventario = {"idObjeto": 5, "cantidad": 1, "idJugador": idJugador};
+        var inventario = {"idObjeto": 5, "cantidad": cantidad5, "idJugador": idJugador};
         console.log(inventario);
         compraObjeto(inventario);
     });
     $('#jabon').click(function () {
-        var inventario = {"idObjeto": 6, "cantidad": 2, "idJugador": idJugador};
+        var inventario = {"idObjeto": 6, "cantidad": cantidad6, "idJugador": idJugador};
         console.log(inventario);
         compraObjeto(inventario);
     });
@@ -90,13 +107,12 @@ $(document).ready(function() {
             url: BASE_URI.concat("/compra"),
             headers: {'content-type': 'application/json', "x-kii-appid": "XXXXX", "x-kii-appkey": "XXXXX"},
             data: JSON.stringify(inventario),
-            dataType: 'json',
+            //dataType: 'json',
             success: function () {
                 alert("Objeto comprado con exito")
             },
             error: function (e) {
                 console.log(e.message);
-                alert("Objeto comprado con exito")
             }
         });
     }
