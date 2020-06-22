beecd0af0d8269fc29cd477662bf1c7bfc5b1485
irega97 <ivanreq97@gmail.com>
2020-06-22 21:11:50 +0200

    ahora si joder, puto mapa

--- public/JavaScript/home.js
+++ public/JavaScript/home.js
@@ -1,5 +1,5 @@
 var token = sessionStorage.getItem("token")
 if (token == null){
-    var url = "http://localhost:8080/login.html";
+    var url = "http://147.83.7.203:8080/login.html";
     window.open(url, "_self");
 }
